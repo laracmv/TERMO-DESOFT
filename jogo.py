250a103bfec31607ef872eb69f64e9ae9533e802
Lara Melo <cmvlara1@gmail.com>
2023-11-01 16:03:33 -0300

    Apresentacao LARA

---
+++ jogo.py
@@ -0,0 +1,14 @@
+from inicializa import inicializa
+
+print("*"*23)
+print("-"*23)
+print("- BEM VIADOS AO TERMO! - ")
+print("*"*23)
+print("-"*23)
+print("\nCansou? digite: cansei")
+print("\nRegras: ")
+print("\n - Você tem \033[0;31;40m n \033[m tentivas para acertas uma palavra aleatória de 5 letras.")
+print(" - A cada tentativa, a palavra testada terá suas letras coloridas conforme:")
+print("  ➔ \033[1;49;36m Azul \033[m : a letra está na posição correta;")
+print("  ➔ \033[1;49;33m Amarelo \033[m : a letra está na posição correta;")
+print("  ➔ \033[2;49;39m Cinza \033[m : a letra está na posição correta;")
