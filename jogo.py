78f37a7a91c3651653e61fe753af35061eb6af0c
Lara Melo <cmvlara1@gmail.com>
2023-11-03 20:00:46 -0300

    loop tentativas e loop jogar de novo LARA

--- jogo.py
+++ jogo.py
@@ -9,84 +9,98 @@ print("- BEM VINDOS AO TERMO! - ")
 print("*"*23)
 print("-"*23)
 print("\nRegras: ")
-print("\n - Você tem \033[0;31;40m 6 \033[m tentativas para acertar uma palavra aleatória de 5 letras.")
+print("\n - Você pode escolher entre 4, 5, 6 ou 7 letras para acertar uma palavra aleatória!")
+print(" - Você tem o número de letras + 1 de tentativas para acertar!")
 print(" - A cada tentativa, a palavra testada terá suas letras coloridas conforme:")
-print("  ➔ \033[1;49;36m Azul \033[m : A letra está na posição correta;")
-print("  ➔ \033[1;49;33m Amarelo \033[m : Tem a letra na palavra mas, não está na posição correta;")
-print("  ➔ \033[2;49;39m Cinza \033[m : A letra não existe nesta palavra;")
-print("Caso não queria continuar a jogar digite: \033[0;31;40m cansei \033[m")
+print("   ➔ \033[1;49;36m Azul \033[m : A letra está na posição correta;")
+print("   ➔ \033[1;49;33m Amarelo \033[m : Tem a letra na palavra mas, não está na posição correta;")
+print("   ➔ \033[2;49;39m Cinza \033[m : A letra não existe nesta palavra;")
+print(" - Os acentos são ignorados.")
+print(" - As palavras podem possuir letras repetidas.")
+print("Caso não queria continuar a jogar digite: \033[1;49;31m cansei \033[m")
 print("\nBora começar o jogo !!\n")
 
-#definir nossos paramêtros
 
-#5 letras no total
-numerodeletras = 5
 
-#delimitar as tentaivas
-tentativas = 0
+jogardenovo = ""
+#Loop para escolher se que ou não jogar de nv
+while jogardenovo != "N" and jogardenovo != "n":
 
-#lista de palavras com 5 letras
-listatermos = filtra(base_palavras.PALAVRAS, numerodeletras)
+    #5 letras no total
+    numerodeletras = int(input(("Digite quantas letras você quer jogar, pode ser 4, 5, 6 ou 7: ")))
 
-#dicionario com nº de palavras, palavra sorteada, especulacoes e nº de tentativas
-dicinicializa = inicializa(listatermos)
-print(dicinicializa) #para mostrar a palavra sorteada, só de teste
+    #lista de palavras com 5 letras
+    listatermos = filtra(base_palavras.PALAVRAS, numerodeletras)
 
-#o loop de acordo com as regras
-#limite de 6 tentativas
-while tentativas < 6:
-    palpite = input('Digite seu palpite: ')
+    #dicionario com nº de palavras, palavra sorteada, especulacoes e nº de tentativas
+    dicinicializa = inicializa(listatermos)
+    print(dicinicializa) #para mostrar a palavra sorteada, só de teste
 
-    #padroniza o palpite em minúsculo
-    palpite = palpite.lower()
+    #delimitar as tentativas
+    tentativas = dicinicializa["tentativas"]
 
-    #caso cansar do jogo
-    if palpite.lower() == "cansei":
-        print('\n>>> Mas já cansou? Até mais, volte sempre.')
-        break
+    palpite = ""
+    f = 0
+    # Numero tentativas -> numero de vezes que o jogador tentou
+    numero_tentativas = 0
+    while (tentativas > 0 and tentativas <= dicinicializa["tentativas"]) and  (palpite != dicinicializa["sorteada"]) and (palpite != "cansei") and (palpite != "cansei mesmo"):
+        palpite = input('Digite seu palpite: ')
 
-    #se o tamanho do palpite for diferente do numero de letras permitido
-    if len(palpite) != numerodeletras: 
-        print('\nNúmero de letras inválido, a palavra precisa conter 5 letras.')
-        continue
+        #padroniza o palpite em minúsculo
+        palpite = palpite.lower()
+        print(dicinicializa["especuladas"])
+        if palpite in dicinicializa["especuladas"]:
+            print("Essa palavra já foi dita, diga outra! ")
+        
+        #se o tamanho do palpite for diferente do numero de letras permitido
+        elif len(palpite) != numerodeletras: 
+            print(f"\nNúmero de letras inválido, a palavra precisa conter {dicinicializa['n']} letras.")
+        
+        elif palpite not in base_palavras.PALAVRAS:
+            print("Palavra desconhecida, tente outra")
 
-    #se palpite for igual a sorteada, ganha o jogo.
-    if palpite == dicinicializa["sorteada"]:
-        print('\n>>> Você acertou, parabéns !!\n')
-        break
+        #caso cansar do jogo
+        elif palpite == "cansei":
+            palpite = input("Não seja um mal perdedor, você tem certeza? [cansei mesmo/ N]: ")
+            if palpite == "cansei mesmo":
+                print('\n>>> Então tá bom, volte sempre.')
 
-    #posição das letras e cores.
-    dicinicializa["especuladas"] += palpite
-    listaposicao = inidica_posicao(dicinicializa["sorteada"], palpite)
-    print(listaposicao)
-
-    a = ''
-    i = 0
-    for letra in palpite:
-        if listaposicao[i] == 0:
-            a+=f"\033[1;49;36m {letra} \033[m"
-        elif listaposicao[i] == 1:
-            a+=f"\033[1;49;33m {letra} \033[m"
-        else: 
-            a+=f"\033[2;49;39m {letra} \033[m"
-        i+=1
-    print(a)
-
-    tentativas +=1
-    print(f'\nFaltam {6-tentativas} tentativas')
-
-    #quando acabar as tentativas
-    if tentativas >= 6 and palpite != dicinicializa["sorteada"]:
-        print(f'\n>>> Você perdeu, a palavra sorteada era: \033[1;49;36m{dicinicializa["sorteada"]}\033[m')
-
-#essa parte do continuar que eu não sei o que fazer nessa merda
-    if tentativas >=6:
-        continuar = input('\nDeseja continuar a jogar s/n: ')
-        if continuar == 's':
-            continue
         else:
-            print('>>> Até logo, volte sempre.')
-            break
+            numero_tentativas += 1
+            #se palpite for igual a sorteada, ganha o jogo.
+            if palpite == dicinicializa["sorteada"]:
+                print(f"\n>>> Você acertou depois de {numero_tentativas} tentativa, parabéns !!!\n")
+            else: 
+                #adiciona os palpites a uma lista dentro de dicinicializa["especuladas"]
+                dicinicializa["especuladas"].append(palpite)
+                listaposicao = inidica_posicao(dicinicializa["sorteada"], palpite)
+                print(listaposicao)
+
+                a = ''
+                i = 0
+                for letra in palpite:
+                    if listaposicao[i] == 0:
+                        a+=f"\033[1;49;36m {letra} \033[m"
+                    elif listaposicao[i] == 1:
+                        a+=f"\033[1;49;33m {letra} \033[m"
+                    else: 
+                        a+=f"\033[2;49;39m {letra} \033[m"
+                    i+=1
+                print(a)
+
+                # Diminui o numero de tentativas ate chegar em 0
+                f+=1
+                tentativas = dicinicializa["tentativas"] - f
+                if tentativas == 0:
+                    print(f'\n>>> Você perdeu, a palavra sorteada era: \033[1;49;36m{dicinicializa["sorteada"]}\033[m')
+                else: 
+                    print(f'\nFaltam {tentativas} tentativas')
+    
+    jogardenovo = input("Gostaria de jogar de novo? [S/N]: ")
+       
+print("Poxa :( até a próxima! ")
+
+
 
 
 
